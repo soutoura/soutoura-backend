396f342e31e77c45c8f9837e7e045554a876c542
jimlecracken13 <jimlecracken13@gmail.com>
2025-09-21 12:14:32 +0000

    first commit"
    ;
    "

---
+++ src/main/java/com/groupe/soutoura_backend/SoutouraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupe.soutoura_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutouraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoutouraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe/soutoura_backend/SoutouraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe.soutoura_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoutouraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
