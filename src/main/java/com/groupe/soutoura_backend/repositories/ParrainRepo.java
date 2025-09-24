16402c248f3017c6178ca0bf66ea0ce8ca0fa9de
Ramatouu <ramatouyayakonare@gmail.com>
2025-09-24 02:54:00 +0000

    Initial commit for parrain branch avec les endpoints de notification et parrain

--- src/main/java/com/groupe/soutoura_backend/repositories/ParrainRepo.java
+++
@@ -1,7 +0,0 @@
-package com.groupe.soutoura_backend.repositories;
-
-import com.groupe.soutoura_backend.models.Parrain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParrainRepo extends JpaRepository<Parrain, Integer> {
-}
